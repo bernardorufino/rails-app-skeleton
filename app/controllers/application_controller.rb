0d6470a3f77fd77eb9ce177eadd58652f654c936
Bernardo Rufino <contato@bernardorufino.com.br>
2012-12-19 16:35:26 -0200

    Page support added with titles.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,6 @@
+# TODO: Bootstrap it
+# TODO: Test?
+# TODO: Minimzr
 class ApplicationController < ActionController::Base
   protect_from_forgery
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,14 @@
 module ApplicationHelper
+  TITLE = "Base Title";
+
+  def title
+    (content_for?(:title)) ? "#{TITLE} | #{content_for(:title)}" : TITLE;
+  end
+
+  def flash_messages
+    flash.to_a.inject("".html_safe) do |code, (type, message)|
+      code + content_tag(:p, message, class: "alert alert-#{type}")
+    end
+  end
+
 end
